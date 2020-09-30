3158d7be5615c1c1f534b7c3f30c8a19c3f20299
Isaiah Branch <idbranch12@gmail.com>
2020-09-30 14:17:36 -0500

    Created a new class: MapForItinerary

---
+++ GitUseExample7920/MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample7920
+//
+//  Created by Branch, Isaiah D on 9/30/20.
+//  Copyright © 2020 Branch, Isaiah D. All rights reserved.
+//
+
+import Foundation
