799f68a6718b097f3aa5ac457dde75c04a9ac7e4
Isaiah Branch <idbranch12@gmail.com>
2020-09-30 13:46:12 -0500

    Initial Commit

---
+++ GitUseExample7920/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample7920
+//
+//  Created by Branch, Isaiah D on 9/30/20.
+//  Copyright © 2020 Branch, Isaiah D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
